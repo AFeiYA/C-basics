1784b9cfb6e2ebf563318bca5536dd27d51410fb
AFeiYA <386665678@qq.com>
2019-07-07 21:25:52 +0800

    pointer02

--- main.cpp
+++ main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <cstring>
-#include "math.h"
 using namespace std;
-
-float DistanceBetweenToPoints(float x1,float y1,float x2,float y2)
-{
-    float Distance = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-    return Distance;
-}
-
-
 
 main()
 {
-    char ch1 = 'a';
-    char *p1 = &ch1;
 
-    cout<<"original ch1 : "<<ch1<<endl;
-   
-    *p1='m';
-    cout<<"modified ch1 : "<<ch1<<endl;
-    cout<<"p1's address : "<<&p1<<endl;
-    cout<<"the value of *p1 : "<<*p1<<endl;
+    //
+    char ch1[20] = "abcde";
+    //ch1 value to *pc1
+    char *pc1 = ch1;
+
+    cout<<"ch1 started address : "<<&ch1<<endl;
+    cout<<"pc1's address       : "<<&pc1<<endl;
+
+
+    ++pc1;
+    cout<<"next value of pc1   : "<<pc1<<endl;
+    cout<<"pc1's address       : "<<&pc1<<endl;
+    cout<<"ch1's address       : "<<&ch1<<endl;
+
+    ++pc1;
+    cout<<"next value of pc1   : "<<pc1<<endl;
+    cout<<"pc1's address       : "<<&pc1<<endl;
+    cout<<"ch1's address       : "<<&ch1<<endl;
+
+    cout<<"next value of ch1   : "<<ch1<<endl;
+    cout<<"next value of pc1   : "<<pc1<<endl;
+
+    cout<<"original ch1 of ch1 : "<<ch1<<endl;
+
+    *pc1='m';
+    
+    cout<<"modified ch1 of ch1 : "<<ch1<<endl;
+    cout<<"the value of pc1    : "<<pc1<<endl;
+
+    cout<<"the size of pc1     : "<<sizeof(ch1)<<endl;
+    cout<<"the size of pc1     : "<<sizeof(pc1)<<endl;
+
+    for(int i=0; ch1[i]; i++)
+    {
+        cout<<ch1[i]<<endl;
+    }
+
 }
 
