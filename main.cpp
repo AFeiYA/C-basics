c926b49121b344c8972005db649f665b146e3d3d
AFeiYA <386665678@qq.com>
2019-07-08 16:24:40 +0800

    string func 01

--- main.cpp
+++ main.cpp
@@ -2,40 +2,59 @@
 #include <cstring>
 using namespace std;
 
-void Swap(char *s1, char *s2)
-{
-    char temp = *s1;
-    *s1 = *s2;
-    *s2 = temp;
-}
 
 
 main()
 {
-    char s1='a';
-    char s2='b';
-    cout<<s1<<s2<<endl;
-    Swap(&s1, &s2);
-    cout<<s1<<s2<<endl;
+    char s1[100] = "1234567";
+    char s2[100] = "abcdefg";
+    char s3[100] = "AFCD";
+    strcat(s1, s2);
+    cout<<s1<<endl;
+    strncat(s1, s2, 5);
+    cout<<s1<<endl;
+    strncpy(s1,s3,4);
+    cout<<s1<<endl;
+    strncpy(s1,s3,5);
+    cout<<s1<<endl;
+    char *p = strchr(s1, 'C');
 
-    int **pp;
-    int ***ppp;
-    int *p;
-    int n=123456;
-    p = &n;
-    pp = &p;
-    ppp = &pp;
+    cout<<&p<<endl;
+    if(p){
+        cout<<p-s1<<*p<<endl;
+    }
+    else{
+        cout<<*p<<endl;
+    }
 
-    cout<<"n's address:\t"<<p<<endl;
-    cout<<"p's address:\t"<<pp<<endl;
-    cout<<"pp's address:\t"<<ppp<<endl;
-    cout<<"ppp's adress:\t"<<&ppp<<endl;
+    p=strstr(s1, "FC");
 
-    cout<<"pp's address:\t"<<ppp<<endl;
-    cout<<"p's address\t"<<*ppp<<endl;
-    cout<<"n's address:\t"<<**ppp<<endl;
-    cout<<"n's value:\t"<<***ppp<<endl;
-    cout<<"n's value:\t"<<**pp<<endl;
-    cout<<"n's value:\t"<<*p<<endl;
+    cout<<&p<<endl;
+    if(p){
+        cout<<p-s1<<p<<endl;
+    }
+    else{
+        cout<<*p<<endl;
+    }
+
+    char str1[] = "=This---is =,a ---test string for strtok,-go-!";
+    p = strtok(str1, " =,!-");
+    cout<<"p's value:"<<p<<",\t"<<"Str1's value:"<<str1<<endl;
+    p = strtok(NULL, " =,!-");
+    cout<<"p's value:"<<p<<",\t"<<"Str1's value:"<<str1<<endl;
+
+
+    int i=0;
+    while(p){
+        cout<<p<<" ";
+        i++;
+        p = strtok(NULL, " =,!-");
+    }
+    cout<<"!"<<endl;
+
+
+
+
+
 }
 
