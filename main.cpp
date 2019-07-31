99504db9593c74381fdbed23c7e4a6335fa9d4f5
AFeiYA <386665678@qq.com>
2019-07-31 20:40:12 +0800

    algorithm 06 recusive algorithm 递归：逆波兰表达式

--- main.cpp
+++ main.cpp
@@ -1,54 +1,34 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 /*
 Algorithm
 递归
-N皇后问题：
-在NxN的棋盘上，
-摆放n个皇后，
-使得任意两个皇后
-    不在同一行，
-    不在同一列，
-    不在正方形的对角线上。
+逆波兰表达式
+算式：（5+3-2）*8的逆波兰表达式为
+* - + 5 3 2 8
  */
 //
 
-int N=10 ;//棋盘矩阵。
-
-int queenPos[100];
-
-void NQueen(int k)//假设k-1个皇后已经摆放完毕。继续摆放第k个皇后。
+double exp()
 {
-    int i;
-    if(k==N)//如果k=n,那么N个皇后已经摆放完毕，输出结果。
+    char s[20];
+    cin>>s;
+    switch(s[0])
     {
-        for(i=0; i<N; i++)
-            cout<<queenPos[i]+1<<" ";
-        cout<<endl;
-        return;
+        case '+' :return exp()+exp();
+        case '-' :return exp()-exp();
+        case '*' :return exp()*exp();
+        case '/' :return exp()/exp();
+        default: return atof(s);
+        break;
     }
-    for(i=0; i<N; ++i)
-    {//遍历第k个皇后所有可能的位置。
-        int j;
-        for(j=0; j<k;++j)
-        {//和已经摆好的k个皇后的位置进行比较。
-            if(queenPos[j]==i||abs(queenPos[j]-i)==abs(k-j))
-            {
-                break;//冲突， 测试下一个位置。
-            }
-        }
-        if(j==k)
-        {
-            queenPos[k]=i;
-            NQueen(k+1);
-        }
-    }
-
 }
+
+
 
 int main()
 {
-    cin>>N;
-    NQueen(0);
+    cout<<exp();
     return 0;
 }
