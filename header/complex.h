84d99d52d43f77f6c966c92371d17cc9a822a716
AFeiYA <386665678@qq.com>
2020-02-19 21:52:10 +0800

    添加注释说明。

--- header/complex.h
+++ header/complex.h
@@ -13,14 +13,14 @@ public:
     complex(T r=0, T i=0)
     :re(r), im(i) //初值话
     {}  //构造函数 创建对象无法被调用 默认值为0，0；如果写成{re=r, im=i},但是属于赋值阶段，程序运行效率会下降
-    complex& operator += (const complex&);
-    T real() const {return re;}
-    T imag() const {return im;}
+    complex& operator += (const complex&); //返回值complex&  
+    T real() const {return re;} //对于只是拿数据，不会改变数据，必须加上const
+    T imag() const {return im;}  //否则 用户定义 const complex c1（2，1） 有可能更改c1的值
 
 private:
     T re, im; 
-    friend complex& __doapl (complex*, const complex&);
-
+    friend complex& __doapl (complex*, const complex&); //firend友元  __doapl可以拿
+    //friend可以直接拿数据。 也可以通过函数拿，速度会慢一些，在封装和效率中寻找平衡点
 };
 
 //类定义
