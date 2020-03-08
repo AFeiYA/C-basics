d365f5a442bfe4ab5f1a73caef280123860985d5
AFeiYA <386665678@qq.com>
2020-03-08 20:26:14 +0800

    complex 形式

--- header/complex.h
+++ header/complex.h
@@ -3,27 +3,63 @@
 #define __COMPLEX__
 
 //前置声明
-
-template<typename T> //
+#include <iostream>
+class complex;
 
 //类声明 class declarations;
 class complex  //class head
 { //class body
 public:
-    complex(T r=0, T i=0)
-    :re(r), im(i) //初值话
+    complex(double r=0, double i=0)
+    :re(r), im(i) //初值化
     {}  //构造函数 创建对象无法被调用 默认值为0，0；如果写成{re=r, im=i},但是属于赋值阶段，程序运行效率会下降
     complex& operator += (const complex&); //返回值complex&  
-    T real() const {return re;} //对于只是拿数据，不会改变数据，必须加上const
-    T imag() const {return im;}  //否则 用户定义 const complex c1（2，1） 有可能更改c1的值
+    double real() const {return re;} //对于只是拿数据，不会改变数据，必须加上const
+    double imag() const {return im;}  //否则 用户定义 const complex c1（2，1） 有可能更改c1的值
 
 private:
-    T re, im; 
-    friend complex& __doapl (complex*, const complex&); //firend友元  __doapl可以拿
+    double re, im; 
+    friend complex& __doapl (complex* ths, const complex& r) ;//firend友元  __doapl可以拿
     //friend可以直接拿数据。 也可以通过函数拿，速度会慢一些，在封装和效率中寻找平衡点
+
 };
+inline complex& 
+complex::operator += (const complex& r) //成员函数
+{
+    return __doapl(this, r);
+}
+inline double
+imag (const complex& x)
+{
+  return x.imag ();
+}
+inline double
+real (const complex& x)
+{
+  return x.real ();
+}
+
+inline complex& 
+__doapl(complex* ths, const complex& r)
+{
+    ths->re += r.re;
+    ths->im += r.im;
+    return *ths;
+}
+
+
+inline complex 
+operator+(const complex& x, const complex& y)
+{
+    return complex (real(x)+real(y), imag(x)+imag(y));
+}
+
+std::ostream& 
+operator << (std::ostream& os, const complex& x )
+{
+    return  os<< '(' << real(x) << ','<<imag(x)<<')';
+}
+
 
 //类定义
-
-
 #endif
--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 
-
 int main()
 {
-    complex<int> c1(2,1);
-    cout<<c1.real()<<c1.imag();
+    complex c1(2,1);
+    cout<<c1.real()<<endl<<c1.imag()<<endl;
+    complex c2(2.0,1.5);
+    cout<<c2.real()<<endl<<c2.imag()<<endl;
+    cout<<c1<<endl;
+    cout<<"HELLLO";
     return 0;
 }
