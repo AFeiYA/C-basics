26c7f3e2a90e087a9fd56497ad4f3625e0af2a1e
LucaZhou <386665678@qq.com>
2020-05-28 14:11:43 +0800

    FE_*

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
+
 #include <iostream>
 #include "header/complex.h"
+#include "header/fegetground.h" 
 using namespace std;
 
 
 int main()
 {
-    complex c1(2,1);
-    cout<<c1.real()<<endl<<c1.imag()<<endl;
-    complex c2(2.0,1.5);
-    cout<<c2.real()<<endl<<c2.imag()<<endl;
-    cout<<c1<<endl;
-    cout<<"HELLLO";
-    return 0;
+    fegetgroundtest();
+    printf("rint(2.6)=%.1f\n", rint(2.6));
+    printf("rint(2.2)=%.1f\n", rint(2.2));
+    printf("rint(3.5)=%.1f\n", rint(3.5));
+    printf("rint(3.6)=%.1f\n", rint(3.6));
+	return 0;
 }
