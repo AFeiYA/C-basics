e943086fe4f2a998f3c3d4e9c6e220043d77f3ac
AFeiYA <386665678@qq.com>
2020-09-20 00:21:23 +0800

    regex

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-#include "header/complex.h"
+#include <regex>
+#include <iterator>
 using namespace std;
+
 
 
 int main()
 {
-    complex c1(2,1);
-    cout<<c1.real()<<endl<<c1.imag()<<endl;
-    complex c2(2.0,1.5);
-    cout<<c2.real()<<endl<<c2.imag()<<endl;
-    cout<<c1<<endl;
-    cout<<"HELLLO";
+    string regSrc = "  11       TPPL  5655  123   100  0  ";
+    //string regSrc = "  11  2   5655.120  -123.456    100.002  ";
+
+    cout<<regSrc<<endl;
+    //regex  self_reg("REGULAR_EXPRESSIONS", regex_constants::ECMAScript|regex_constants::icase);
+
+    regex word_regex("([-|\\d]+\\.+\\d+)|(\\d+)");
+    regex face_regex("(\\w+)");
+
+    auto words_begin = sregex_iterator(regSrc.begin(),regSrc.end(),face_regex);
+    auto words_end = sregex_iterator();
+    for(sregex_iterator i=words_begin; i!=words_end;++i){
+        std::smatch match = *i;
+        string match_str =  match.str();
+        cout<<match_str.length()<<endl;
+    }
+
     return 0;
+    
 }
